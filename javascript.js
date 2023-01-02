1381d9ce8660d8bfb0579f701ef6a5f210303346
Bapteds <112860971+Bapteds@users.noreply.github.com>
2023-01-02 17:13:02 +0100

    Test2

---
+++ javascript.js
@@ -0,0 +1,23 @@
+//###--ANOTATIONS--###\\
+
+//JAVA for (initialisation; terminaison; increment) 
+//JAVA SCRIPT for ([expressionInitiale]; [condition]; [expressionIncrément])
+//let declaration d'une variable en local. Si x= 1 dans une fonction, la méthode main peut avoir un x=2, il ne rentre pas en conflit avec x=1
+//Forme en JS https://www.youtube.com/watch?v=uq66IuqYdWg
+
+
+//###--CODE--###\\
+
+test2=0;
+test1=1;
+
+
+function testfonct(){
+
+    let canvas = document.getElementById('c1');
+    let ctx = canvas.getContext('2d');
+    
+    ctx.strokeStyle = '#4444CC'; //Nuance de bleu
+    ctx.strokeRect(10, 25, 200, 100);
+}
+
